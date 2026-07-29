68968507644d032f13d8320ee51f99969c6435a0
Simon Spoor <sammoore359@gmail.com>
2026-07-29 13:51:18 -0700

    bug fix

--- web_interface/ros_bridge.js
+++ web_interface/ros_bridge.js
@@ -9,36 +9,46 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     document.getElementById("emergency-stop").onclick = emergencyStop
 
-    // Check for emergency stop
-    if (!stopAll) {
-        // Connect to ROS
-        function connectROS() {
-            const url = document.getElementById('connection-port').value
-            const dataInput = document.getElementById("connection-status-text")
-            const indicator = document.getElementById("connection-status-indicator")
-
-            console.log("A")
-
-            var ros = new ROSLIB.Ros({
-                url: url
-            });
-
-            ros.on('connection', function () {
-                dataInput.textContent = "Connected"
-                indicator.style.backgroundColor = 'var(--accent-active)';
-            })
-
-            ros.on('error', function () {
-                dataInput.textContent = "Error"
-                indicator.style.backgroundColor = 'var(--accent-mid)';
-            })
-
-            ros.on('close', function () {
-                dataInput.textContent = "Disconnected"
-                indicator.style.backgroundColor = 'var(--accent-inactive)';
-            })
-        }
-
-        document.getElementById("connect-button").onclick = connectROS;
+function checkStop() {
+    if (stopAll) {
+        console.log("Emergency Stop");
+        return true;
     }
+    return false;
+}
+
+ 
+    // Connect to ROS
+    function connectROS() {
+        const url = document.getElementById('connection-port').value
+        const dataInput = document.getElementById("connection-status-text")
+        const indicator = document.getElementById("connection-status-indicator")
+
+        if (checkStop()) return;
+
+
+        console.log("A")
+
+        var ros = new ROSLIB.Ros({
+            url: url
+        });
+
+        ros.on('connection', function () {
+            dataInput.textContent = "Connected"
+            indicator.style.backgroundColor = 'var(--accent-active)';
+        })
+
+        ros.on('error', function () {
+            dataInput.textContent = "Error"
+            indicator.style.backgroundColor = 'var(--accent-mid)';
+        })
+
+        ros.on('close', function () {
+            dataInput.textContent = "Disconnected"
+            indicator.style.backgroundColor = 'var(--accent-inactive)';
+        })
+    }
+
+    document.getElementById("connect-button").onclick = connectROS;
+
 });
